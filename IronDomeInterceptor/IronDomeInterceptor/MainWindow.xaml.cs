5cb812f43b8423f130f742beb17ee82db519a6f2
JoeyMan09 <idankislev@gmail.com>
2026-09-03 12:34:52 +0300

    ++

--- IronDomeInterceptor/IronDomeInterceptor/MainWindow.xaml.cs
+++ IronDomeInterceptor/IronDomeInterceptor/MainWindow.xaml.cs
@@ -12,9 +12,6 @@ using IronDomeInterceptor.inteceptor;
 
 namespace IronDomeInterceptor
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         private InterceptorClient interceptorClient;
